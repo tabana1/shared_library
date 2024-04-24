43503c89679f1c22fcbf1791a20d199dd58e27c5
Mohamed Wael Tabana <113201941+tabana1@users.noreply.github.com>
2024-04-25 01:54:43 +0200

    Delete vars/editNewImage.groovy

--- vars/editNewImage.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-// KubernetesCredentialsID 'KubeConfig file'
-def call(String imageName) {
-    
-    // Edit deployment.yaml with new Docker Hub image
-    sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
-
-}
